ec0ece51dce7866dc129b97d92afff85c0a7124b
light-planck <seseifunc16@gmail.com>
2023-11-01 10:51:03 +0900

    feat: メモ追加機能を追加

--- app.rb
+++ app.rb
@@ -3,10 +3,19 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+$memos = []
+
 get '/' do
+  @memos = $memos
   erb :index
 end
 
 get '/new' do
   erb :new
+end
+
+post '/memos' do
+  new_memo = { title: params[:title], content: params[:content] }
+  $memos << new_memo
+  redirect to('/')
 end
